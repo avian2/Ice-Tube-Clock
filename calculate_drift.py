31ab9ccd711bb09fa021e84396c55803dd74b47e
Tomaz Solc <tomaz.solc@tablix.org>
2013-09-21 19:59:09 +0200

    Second measurement results.

--- calculate_drift.py
+++ calculate_drift.py
@@ -1,13 +1,24 @@
 import datetime
 
 # edit these
-ref_1 = datetime.datetime(2013, 9, 13, 23, 26, 0)
-clk_1 = datetime.datetime(2013, 9, 13, 23, 26, 0)
+
+# First measurement
+
+#ref_1 = datetime.datetime(2013, 9, 13, 23, 26, 0)
+#clk_1 = datetime.datetime(2013, 9, 13, 23, 26, 0)
 
 #ref_2 = datetime.datetime(2013, 9, 16, 20, 18, 30)
 #clk_2 = datetime.datetime(2013, 9, 16, 20, 18, 21)
-ref_2 = datetime.datetime(2013, 9, 17, 18, 37, 00)
-clk_2 = datetime.datetime(2013, 9, 17, 18, 36, 48)
+
+#ref_2 = datetime.datetime(2013, 9, 17, 18, 37, 00)
+#clk_2 = datetime.datetime(2013, 9, 17, 18, 36, 48)
+
+# Second measurement
+
+ref_1 = clk_1 = datetime.datetime(2013, 9, 17, 19, 42, 0)
+
+ref_2 = datetime.datetime(2013, 9, 21, 19, 45, 50)
+clk_2 = datetime.datetime(2013, 9, 21, 19, 45, 51)
 
 def total_seconds(td):
 	return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**6
